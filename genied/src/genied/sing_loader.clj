ddeba7da7d371f286b4ac2849d0ef70db69c7a75
ndevreeze <nico@ndv.nl>
2021-03-22 14:03:25 +0100

    Keep list of loaded libraries
    For performance, loading dependencies takes some time, even if they
    are already loaded.

--- genied/src/genied/sing_loader.clj
+++ genied/src/genied/sing_loader.clj
@@ -2,14 +2,26 @@
   (:gen-class))
 
 ;; only define the singleton classloader here, for use from other namespaces.
-(def classloader (atom nil))
+;; dependencies is a set of coordinates, where coordinate is a vec of lib and version.
+(def classloader (atom {:loader nil :dependencies #{}}))
 
 (defn get-classloader
   "Get the globally set dynamic classloader"
   []
-  @classloader)
+  (:loader @classloader))
 
 (defn set-classloader!
-  "Set the globally set dynamic classloader"
+  "Set the globally set dynamic classloader
+   reset set of loaded packages"
   [new-val]
-  (reset! classloader new-val))
+  (reset! classloader {:loader new-val :dependencies #{}}))
+
+(defn has-dep?
+  "Return true if dependency is already loaded"
+  [coord]
+  (contains? (:dependencies @classloader) coord))
+
+(defn add-dep!
+  "Add dependency coordinates to loaded set"
+  [coord]
+  (swap! classloader update-in [:dependencies] conj coord))
