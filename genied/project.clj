209ab04b2717f21dca0cc570f0dd84cadcb25f9e
ndevreeze <nico@ndv.nl>
2021-03-24 14:57:22 +0100

    Replace raynes/fs by clj-commons/fs

--- genied/project.clj
+++ genied/project.clj
@@ -3,10 +3,11 @@
   :url "https://github.com/ndevreeze/genie"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.apache.commons/commons-compress "1.19"] ;; explicitly, raynes/fs uses an old one.
+  :dependencies [
+                 ;; [org.apache.commons/commons-compress "1.19"] ;; explicitly, raynes/fs uses an old one.
                  [org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.cli "1.0.194"]
-                 [me.raynes/fs "1.4.6"]
+                 [clj-commons/fs "1.6.307"]
                  [nrepl "0.8.3"]
                  [clj-commons/pomegranate "1.2.0"] ;; 2021-02-21: for dynamic loading of libraries.
                  [ndevreeze/logger "0.2.0"]
@@ -15,5 +16,4 @@
   :target-path "target/%s"
   :profiles {:dev {:dependencies [[midje "1.9.9"]]}
              :uberjar {:aot :all}})
-
 
