9395c52e0a81a9feaae95e1f36e4cb32a2297489
ndevreeze <nico@ndv.nl>
2021-03-24 21:51:14 +0100

    Add test_head.clj, for testing relative path names.
    The current working dir (cwd) is most probably different from
    server-cwd and might also be different from the script-dir and
    client-dir. We want to make sure a relative path given on te command
    line is found.

---
+++ test/test_head.clj
@@ -0,0 +1,39 @@
+#! /usr/bin/env genie
+
+;; test printing the first few lines of a given file
+;; including test for relative files.
+
+(ns test-head
+  (:require 
+   [ndevreeze.cmdline :as cl]
+   [clojure.string :as str]))
+
+(def cli-options
+  [["-c" "--config CONFIG" "Config file"]
+   ["-h" "--help" "Show this help"]])
+
+;; maybe need doall here.
+(defn head-file
+  "Return the first 5 lines of `path`"
+  [path]
+  (with-open [rdr (clojure.java.io/reader path)]
+    (str/join "\n" (take 5 (line-seq rdr)))))
+
+(defn script [opt arguments ctx]
+  ;;  (println "ctx: " ctx)
+  (println (head-file (first arguments))))
+
+(defn main [ctx args]
+  (cl/check-and-exec "" cli-options script args ctx))
+
+;; for use with 'clj -m test-divide-by-0
+
+;; TODO - maybe need solution if script is called from another
+;;        directory, in which case deps.edn cannot be found, which is
+;;        used for bootstrapping.  or do use the -M option for giving
+;;        the script path?
+(defn -main
+  "Entry point from clj cmdline script. Need to call System/exit, hangs otherwise."
+  [& args]
+  (cl/check-and-exec "" cli-options script args {:cwd "."})
+  (System/exit 0))
