dddf18870af30ee6e5ef9b3efc7e8dbb76909cb5
ndevreeze <nico@ndv.nl>
2021-03-24 20:35:51 +0100

    Test exception handling
    Shows similar output to running with 'clj -m', including faulty line.

---
+++ test/test_divide_by_0.clj
@@ -0,0 +1,34 @@
+#! /usr/bin/env genie
+
+;; test exception handling.
+
+(ns test-divide-by-0
+  (:require 
+   [ndevreeze.cmdline :as cl]))
+
+(def cli-options
+  [["-c" "--config CONFIG" "Config file"]
+   ["-h" "--help" "Show this help"]])
+
+(defn divide-by-0
+  [opt ctx]
+  (println "5 / 0 = " (/ 5 0)))
+
+(defn script [opt arguments ctx]
+  (println "ctx: " ctx)
+  (divide-by-0 opt ctx))
+
+(defn main [ctx args]
+  (cl/check-and-exec "" cli-options script args ctx))
+
+;; for use with 'clj -m test-divide-by-0
+
+;; TODO - maybe need solution if script is called from another
+;;        directory, in which case deps.edn cannot be found, which is
+;;        used for bootstrapping.  or do use the -M option for giving
+;;        the script path?
+(defn -main
+  "Entry point from clj cmdline script. Need to call System/exit, hangs otherwise."
+  [& args]
+  (cl/check-and-exec "" cli-options script args {:cwd "."})
+  (System/exit 0))
