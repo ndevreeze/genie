dcf671ad5e615f9a271aa8b3ff07d14abc80c956
ndevreeze <nico@ndv.nl>
2021-04-06 19:51:26 +0200

    Remove util.clj
    Only had repl-eval, and not using.

--- genied/src/genied/util.clj
+++
@@ -1,8 +0,0 @@
-(ns genied.util
-  (:gen-class)
-  (:require [nrepl.core :as repl]))
-
-(defn repl-eval
-  "Another test function"
-  [ses code]
-  (doall (repl/message ses {:op "eval" :code code})))
