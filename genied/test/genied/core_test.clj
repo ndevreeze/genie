ddb3017052349f557fdd2735f1bcc72fbd46d932
ndevreeze <nico@ndv.nl>
2021-04-23 22:12:48 +0200

    Add comment wrt no tests here
    No real tests for now. Most functions are stateful, so harder to test.
    Main testing is done with the scripts in the test-directory.

--- genied/test/genied/core_test.clj
+++ genied/test/genied/core_test.clj
@@ -1,4 +1,6 @@
 (ns genied.core-test
+  "No real tests for now. Most functions are stateful, so harder to test.
+   Main testing is done with the scripts in the test-directory."
   (:require [clojure.test :refer :all] ;; use standard test...
             [midje.sweet :as midje]    ;; ... or midje.
             [genied.core :refer :all]))
