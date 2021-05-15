aa235153a782ebf6615d3cd71e98baa8929725ba
ndevreeze <nico@ndv.nl>
2021-05-15 20:34:46 +0200

    Add with-src-dir
    Testcase similar to normal clj script, with a src-dir.

---
+++ test/with-src-dir/src/test_dyn_cl.clj
@@ -0,0 +1,43 @@
+#! /usr/bin/env genie.clj
+
+(ns test-dyn-cl
+  "specific script for testing the classloader on the client side."
+  (:require
+   [ndevreeze.cmdline :as cl]
+   [clojure.data.csv :as csv]))
+
+(def cli-options
+  "Cmdline options"
+  [["-c" "--config CONFIG" "Config file"]
+   ["-h" "--help" "Show this help"]])
+
+(defn data-csv
+  "Parse some csv in a string"
+  []
+  (println "Parsing csv using data.csv: "
+           (csv/read-csv "abc,123,\"with,comma\"")))
+
+(defn script
+  "Main script called by both `main` and `-main`"
+  [_opt _arguments ctx]
+  (println "ctx: " ctx)
+  (data-csv))
+
+;; expect context/ctx now as first parameter, a map.
+(defn main
+  "Main from genie"
+  [ctx args]
+  (cl/check-and-exec "" cli-options script args ctx))
+
+;; for use with 'clj -m test-dyn-cl
+
+;; TODO - maybe need solution if script is called from another
+;;        directory, in which case deps.edn cannot be found, which is
+;;        used for bootstrapping.  or do use the -M option for giving
+;;        the script path?
+(defn -main
+  "Entry point from clj cmdline script.
+  Need to call System/exit, hangs otherwise."
+  [& args]
+  (cl/check-and-exec "" cli-options script args {:cwd "."})
+  (System/exit 0))
