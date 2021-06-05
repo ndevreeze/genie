b7ba9337198a8b29c4b3a5cf03b30e26af4e44d6
ndevreeze <nico@ndv.nl>
2021-06-05 20:58:08 +0200

    Clean up

--- genied/src/genied/core.clj
+++ genied/src/genied/core.clj
@@ -71,39 +71,6 @@
 
   (post-init-daemon opt arguments ctx))
 
-#_(defn do-script
-    "Main user defined function for genied"
-    [{:keys [port config verbose] :as opt} arguments ctx]
-    (when (:verbose opt)
-      (alter-var-root #'diag/*verbose* (constantly true)))
-    (log/init {:location (log-location opt) :name "genied"
-               :level (if verbose :debug :info)})
-    (log/debug "genied started")
-    (log/info "Using config: " config)
-    (log/debug "Opt given: " opt)
-    (log/debug "Rest arguments given: " arguments)
-    (log/debug "Context (ctx): " ctx)
-
-    (diag/print-diagnostic-info "start of do-script (genied)")
-    (loader/init-dynamic-classloader!)
-
-    (log/debug "init dynamic classloader done: " (state/get-classloader))
-    (diag/print-diagnostic-info "after init-dynamic-classloader!")
-
-    (loader/mark-project-libraries)
-    (loader/load-startup-libraries opt)
-
-    (state/set-out-streams! *out* *err*)
-
-    (log/debug "Starting daemon on port " port)
-    (state/set-daemon! (nrepl/start-server :port port))
-    (log/info "nrepl daemon started on port:" port)
-
-    (diag/print-diagnostic-info "after start-server")
-
-    (client/init) ;; dummy for now, this also makes sure the namespace is loaded.
-    (diag/print-diagnostic-info "end of do-script (genied)"))
-
 (defn -main
   "Main function, starting point"
   [& args]
