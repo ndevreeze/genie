4b39867c735347de86345aebf6a8f06d7c5900d2
ndevreeze <nico@ndv.nl>
2021-06-05 20:57:23 +0200

    Add test to call client script
    Just the simplest client script for now, as a PoC.

--- genied/test/genied/core_test.clj
+++ genied/test/genied/core_test.clj
@@ -1,10 +1,21 @@
 (ns genied.core-test
   "No real tests for now. Most functions are stateful, so harder to test.
    Main testing is done with the scripts in the test-directory."
-  (:require [midje.sweet :as midje]))
+  (:require [midje.sweet :as midje]
+            [genied.client :as client]
+            [genied.core :as core]))
+
+;; setup daemon for testing
+(core/pre-init-daemon {} [] {})
+(core/post-init-daemon {} [] {})
 
 (midje/facts
- "Test several facts"
- (midje/fact "Basic test"
-             (* 6 7)
-             => 42))
+ "Test calling exec-script with test-scripts"
+
+ (midje/fact "Test calling test.clj"
+             (with-out-str
+               (client/exec-script "../test/test.clj"
+                                   'test/main
+                                   {:protocol-version "0.1.0"
+                                    :opt {}} []))
+             => "Just a simple line to stdout\nctx:  {:protocol-version 0.1.0, :opt {}}\n"))
