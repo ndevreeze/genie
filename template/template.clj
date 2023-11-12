7e9a954a37a5d5c0540ef6820e996dff599f0a66
ndevreeze <nico@ndv.nl>
2023-11-12 20:56:16 +0100

    Add try-clause to template

--- template/template.clj
+++ template/template.clj
@@ -5,13 +5,16 @@
    [clojure.data.csv :as csv]
    [clojure.edn :as edn]
    [clojure.java.io :as io]            
+   [clojure.java.jdbc :as jdbc]
    [clojure.set :as set]
    [clojure.string :as str]
    [clojure.tools.cli :as cli]
    [java-time :as time]
    [me.raynes.fs :as fs]
    [nrepl.server :as nrepl]
+   [ontodev.excel :as xls]
 
+   [ndevreeze.flexdb :as db]
    [ndevreeze.logger :as log]
    [ndevreeze.cmdline :as cl]))
 
@@ -29,16 +32,21 @@
   (log/debug opt ctx)
   (log/debug "arguments: " arguments)
   (log/debug "ctx: " ctx)
-  ;; add your script here.
+
+  (try
+    ;; add expressions here.
+    (catch Exception e
+      (log/error "Caught error:" e))
+    (finally
+      ;; not sure if finally is needed here.
+      ))
+
   (log/close))
 
 (defn main [ctx args]
   (cl/check-and-exec "" cli-options script args ctx))
 
 ;; call with: 'clj -m {{namespace}}'
-;; call with: 'clj -M {{script}}' does not seem to work.
-;; TODO - maybe need solution if script is called from another directory, in which case deps.edn cannot be found, which is used for bootstrapping.
-;;        or do use the -M option for giving the script path?
 (defn -main
   "Entry point from clj cmdline script. Need to call System/exit, hangs otherwise."
   [& args]
